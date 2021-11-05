b6cf22bff1a79d4a265be3aed92613603dec5f26
Michael Rodriguez <miarodriguezur@unal.edu.co>
2021-11-05 12:07:09 -0500

    S2 C3 3.9 Refactoring, func updateButton is added

--- Section II Cap 3. TDD App Setup/FitNess/FitNess/UI Layer/StepCountController.swift
+++ Section II Cap 3. TDD App Setup/FitNess/FitNess/UI Layer/StepCountController.swift
@@ -64,17 +64,17 @@ class StepCountController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    let title = AppState.notStarted.nextStateButtonLabel
-    startButton.setTitle(title, for: .normal)
-
-    
+    updateButton()     //Refactorizacion con funcion
   }
 
   @IBAction func startStopPause(_ sender: Any?) {
     AppModel.instance.start()
-    let title = AppModel.instance.appState.nextStateButtonLabel
-    startButton.setTitle(title, for: .normal)
-
+    updateButton()    //Refactorización con funcion
   }
   
+  private func updateButton() {
+    let title = AppModel.instance.appState.nextStateButtonLabel
+    startButton.setTitle(title, for: .normal)
+  }
+
 }
